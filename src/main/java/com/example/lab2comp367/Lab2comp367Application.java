d9d5dfa2f2e59534b9728e7fd1aed43fc8c699fb
cmoor <cmoore156@gmail.com>
2023-03-25 12:14:06 -0400

    Initial commit

---
+++ src/main/java/com/example/lab2comp367/Lab2comp367Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2comp367;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2comp367Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2comp367Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab2comp367/Lab2comp367ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2comp367;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2comp367ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
